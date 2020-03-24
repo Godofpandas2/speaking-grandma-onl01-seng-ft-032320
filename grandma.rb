1a4cc18cb5bee48243bc03cea2fa738be50426f6
David Hutchison <dhutchis68516@gmail.com>
2020-03-24 15:29:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (talk)
   if talk == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif talk == talk.upcase
-    return "NO, NOT SINCE 1938"
+    return "NO, NOT SINCE 1938!"
   else return "HUH?! SPEAK UP, SONNY!"
   end
 end
